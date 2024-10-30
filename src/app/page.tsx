9262cc9a49ff5ccd40492a16e5a906375518c8e5
sajee.s <sajee.s@eight25media.com>
2024-10-30 11:57:16 +0530

    Installed contentful rich text types and pull the data from contentful V1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import { createClient } from "contentful";
+import { BlogQueryResult } from "./types";
 
 undefined /* page.tsx */
 
@@ -8,7 +10,7 @@ const client = createClient({
   accessToken: process.env.ACCESS_TOKEN,
 });
 
-const getBlogEntries = async () => {
+const getBlogEntries = async ():Promise<BlogQueryResult> => {
   const entries = await client.getEntries({ content_type: "blog" });
   return entries;
 };
@@ -21,7 +23,24 @@ console.log("Home -> blogEntries", blogEntries)
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>blog listing goes here</p>
+      {blogEntries.items.map((singlePost) => {
+        const { slug, title, date } = singlePost.fields;
+        return (
+          <div key={slug}>
+            <Link href={`/articles/${slug}`}>
+              <h2>{title}</h2>
+              <span>
+                Posted on{" "}
+                {new Date(date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
+            </Link>
+          </div>
+        );
+      })}
     </main>
   )
 }
